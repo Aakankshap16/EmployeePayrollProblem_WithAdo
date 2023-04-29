020b1a7afaa61d4d85e77688f1e243a63c19b6fb
Aakankshap16 <aakankshap971@gmail.com>
2023-04-29 20:14:17 +0530

    Built connection and retrive employee data

--- EmployeeRepo.cs
+++ EmployeeRepo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,14 +14,37 @@ namespace EmployeePayroll
 
         SqlConnection connection = new SqlConnection(connectionstring);
 
-        public bool AddEmployee()
+        public bool AddEmployee(EmployeeModel model)
         {
             try
             {
                 using (this.connection)
                 {
-                    connection.Open();
-                    Console.WriteLine("Database connection established.");
+                    SqlCommand command = new SqlCommand("SpAddemployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@BasicPay ", model.BasicPay);
+                    command.Parameters.AddWithValue("@Duduction", model.Duduction);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", model.StartDate);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@Country", model.Country);
+
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+
                 }
             }
             catch (Exception ex)
