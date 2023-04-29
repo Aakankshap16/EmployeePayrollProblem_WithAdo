b1d10a7612b563120cdb166100c0fdc8c63e0820
Aakankshap16 <aakankshap971@gmail.com>
2023-04-30 02:45:18 +0530

    create table and insert values

--- EmployeePayroll_Sql.sql
+++ EmployeePayroll_Sql.sql
@@ -20,4 +20,10 @@ Country varchar(255)
 
 select * from employee
 
+insert into employee values('Divya','9876543','Sector-5','Software','F',1000,300,500,1200,2500,'2019-5-16','Lucknow','India')
+insert into employee values('Shivam','98567987','Block-8','Software','M',1000,300,500,1200,2500,'2019-5-16','Bhopal','India')
+insert into employee values('terisa','9876543','Sector-5','Software','F',1000,300,500,1200,2500,'2010-9-10','Goa','India')
+
+
+
 
