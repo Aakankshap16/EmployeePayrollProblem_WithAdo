405f635da5ff2669c65ed0d36211a96791b0b63e
Aakankshap16 <aakankshap971@gmail.com>
2023-04-29 20:10:22 +0530

    call UC

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@
         {
             Console.WriteLine("Welcome to Employee Payroll");
             EmployeeRepo employeeRepo = new EmployeeRepo();
-            employeeRepo.AddEmployee();
+
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeName = "Aakanksha Pandey";
+            employee.PhoneNumber = "234567890";
+            employee.Address = "sector-7";
+            employee.Department = "Hr";
+            employee.Gender = 'F';
+            employee.BasicPay = 200000.00;
+            employee.Duduction = 1500.00;
+            employee.TaxablePay = 200.00;
+            employee.Tax = 300;
+            employee.NetPay = 2500.00;
+            employee.StartDate = DateTime.Now;
+            employee.City = "Hydrabad";
+            employee.Country = "India";
+
+
+            employeeRepo.AddEmployee(employee);
+
         }
     }
 }
