800e171db84c05e31f8fc8bdf37d7aededcd4375
Aakankshap16 <aakankshap971@gmail.com>
2023-04-30 02:47:15 +0530

    call UC

--- Program.cs
+++ Program.cs
@@ -22,8 +22,32 @@
             employee.City = "Hydrabad";
             employee.Country = "India";
 
+          //  employeeRepo.AddEmployee(employee);
 
-            employeeRepo.AddEmployee(employee);
+
+            EmployeeModel employee2 = new EmployeeModel();
+            employee2.EmployeeName = "tersia";
+            employee2.PhoneNumber = "9876590";
+            employee2.Address = "Main-St";
+            employee2.Department = "Sales";
+            employee2.Gender = 'F';
+            employee2.BasicPay = 200000.00;
+            employee2.Duduction = 1500.00;
+            employee2.TaxablePay = 200.00;
+            employee2.Tax = 1500;
+            employee2.NetPay = 1500000.00;
+            employee2.StartDate = DateTime.Now;
+            employee2.City = "pune";
+            employee2.Country = "India";
+           
+
+            //employeeRepo.AddEmployee(employee2);
+
+            // Update Terisa's salary
+            employeeRepo.UpdateEmployee(employee2.EmployeeName, 3000000.00);
+
+
+           
 
         }
     }
