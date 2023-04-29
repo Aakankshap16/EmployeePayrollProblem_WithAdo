8d2552286911d831b5987559b46fe086a42b0d5e
Aakankshap16 <aakankshap971@gmail.com>
2023-04-29 20:18:35 +0530

    create table and inset values

--- EmployeePayroll_Sql.sql
+++ EmployeePayroll_Sql.sql
@@ -1,4 +1,23 @@
   
     /* Welcome to EmployeePayRoll in sql */
-Create database PayRollService266
+create table employee
+(
+EmployeeId int primary key identity,
+EmployeeName varchar(355),
+PhoneNumber varchar(255),
+Address  varchar(255),
+Department varchar(255),
+Gender varchar(255),
+BasicPay float,
+Duduction float,
+TaxablePay float,
+Tax float,
+NetPay float,
+StartDate Date,
+City varchar(255),
+Country varchar(255)
+)
+
+select * from employee
+
 
