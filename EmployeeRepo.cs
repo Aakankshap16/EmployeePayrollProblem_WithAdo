23293645368cf2ab602f8bf0964cfe3d93958fb9
Aakankshap16 <aakankshap971@gmail.com>
2023-04-29 19:49:13 +0530

    Built connection

---
+++ EmployeeRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    public class EmployeeRepo
+    {
+        public static string connectionstring = "data source=(localdb)\\MSSQLLocalDB; initial catalog=PayRollService266; integrated security=true";
+
+        SqlConnection connection = new SqlConnection(connectionstring);
+
+        public bool AddEmployee()
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    connection.Open();
+                    Console.WriteLine("Database connection established.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error connecting to database: " + ex.Message);
+            }
+
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+
+    }
+}
