7eb2b6164712e8d9e6e4e13520067567eb83bd88
Aakankshap16 <aakankshap971@gmail.com>
2023-05-01 02:50:55 +0530

    refactor code for getting data by name

--- PayrollDBService.cs
+++ PayrollDBService.cs
@@ -4,53 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
+using System.Security.Cryptography.X509Certificates;
 
 namespace EmployeePayroll
 {
    
     public class PayrollDBService
     {
-        private static readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PayRollService266;Integrated Security=True";
-        private static PayrollDBService instance = null;
-        private static SqlConnection connection = null;
-        private static SqlCommand getEmployeeModelByNameCommand = null;
-
-        private PayrollDBService()
+        private static SqlConnection ConnectionString()
         {
-            connection = new SqlConnection(connectionString);
-            connection.Open();
-            getEmployeeModelByNameCommand = new SqlCommand("SELECT * FROM employee WHERE EmployeeName = @EmployeeName", connection);
-            getEmployeeModelByNameCommand.Parameters.AddWithValue("@EmployeeName", " ");
+            return new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PayRollService266;Integrated Security=True");
         }
-
-        public static PayrollDBService Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new PayrollDBService();
-                }
-                return instance;
-            }
-        }
-
         public EmployeeModel GetEmployeeNameByName(string name)
         {
-            EmployeeModel employee =new EmployeeModel();
-            getEmployeeModelByNameCommand.Parameters["@EmployeeName"].Value = name;
-            SqlDataReader reader = getEmployeeModelByNameCommand.ExecuteReader();
-            if (reader.Read())
-            {
-                employee = new EmployeeModel();
-                employee.EmployeeName = reader["EmployeeName"].ToString();
-                employee.BasicPay = Convert.ToInt32(reader["BasicPay"]);
-                employee.StartDate = Convert.ToDateTime(reader["StartDate"]);
-            }
-            reader.Close();
-           return employee;
-        }
-    }
+            EmployeeModel employee = null;
 
+            using (SqlConnection conn = ConnectionString())
+            {
+                try
+                {
+                    using (SqlCommand getEmployeeModelByNameCommand = new SqlCommand("SELECT * FROM employee WHERE EmployeeName = @EmployeeName", conn))
+                    {
+                        getEmployeeModelByNameCommand.Parameters.AddWithValue("@EmployeeName", " ");
+                        employee = new EmployeeModel();
+                        conn.Open();
+                        getEmployeeModelByNameCommand.Parameters["@EmployeeName"].Value = name;
+                        SqlDataReader reader = getEmployeeModelByNameCommand.ExecuteReader();
+                      
+                        if (reader.Read())
+                        {
+                            employee.EmployeeName = reader["EmployeeName"].ToString();
+                            employee.BasicPay = Convert.ToInt32(reader["BasicPay"]);
+                            employee.StartDate = Convert.ToDateTime(reader["StartDate"]);
+                        }
+                        reader.Close();
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error connecting to database: " + ex.Message);
+                }
+            }
+
+            return employee;
+        }
+
+
+    }
 }
+
+
